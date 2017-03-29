f746c2db8c7fd31962a0bb15491ce95282a6c342
Luiz Reginaldo <luizreginaldogg@hotmail.com>
2017-03-28 22:08:35 -0300

    Até Media 2

--- uri.c
+++ uri.c
@@ -2,11 +2,27 @@
 
 int main(void){
 
-
-    double raio; 
+    
+    
+      
+    /*double notaA,notaB,notaC;
+    scanf("%lf %lf %lf",&notaA,&notaB,&notaC);
+    double MEDIA = notaA*0.2 + notaB*0.3 + notaC*0.5;
+    printf("MEDIA = %.1f\n",MEDIA);
+    
+    /*
+    double nota1;
+    double nota2;
+    scanf("%lf %lf",&nota1,&nota2);
+    double MEDIA = nota1*0.35 + nota2*0.75;
+    MEDIA = MEDIA/1.1;
+    printf("MEDIA = %.5f\n",MEDIA);
+    return 0;
+    
+    /*double raio; 
     double pi=3.14159;
     scanf("%lf",&raio);
     double area = pi * raio * raio;
     printf("A=%.4f\n",area);
-    return 0;
+    return 0;*/
 }
